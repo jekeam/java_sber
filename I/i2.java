d515742d5067e447670f6ba12a0104052f9b0d03
Ubuntu <ubuntu@ip-172-31-24-151.us-east-2.compute.internal>
2020-02-06 10:27:49 +0000

    3 step

--- I/i2.java
+++ I/i2.java
@@ -1,6 +1,6 @@
 public class i2 {
     public static void main(String[] args) {
         System.out.println("«We have to get out of here»");
-        System.out.println(" «We need transport»");
+        System.out.println("«We need transport»");
     }
 }
---
+++ I/i3.java
@@ -0,0 +1,10 @@
+public class i2 {
+    public static void main(String[] args) {
+        String s = "Destroy Droidekas!";
+        Integer x = 1;
+        while (x <= 5){
+            x += 1;
+            System.out.println(s);
+        }
+    }
+}
