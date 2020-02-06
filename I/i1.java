87c7bfa32cfa7d653aba7a0259a6a1e6400e4004
Ubuntu <ubuntu@ip-172-31-24-151.us-east-2.compute.internal>
2020-02-06 10:22:35 +0000

    first commit

---
+++ I/i1.java
@@ -0,0 +1,5 @@
+public class i1 {
+    public static void main(String[] args) {
+        System.out.println("Mesa called Jar Jar Binks, mesa your humble servant!");
+    }
+}
---
+++ I/i2.java
@@ -0,0 +1,6 @@
+public class i2 {
+    public static void main(String[] args) {
+        System.out.println("«We have to get out of here»");
+        System.out.println(" «We need transport»");
+    }
+}
